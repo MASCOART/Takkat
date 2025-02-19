1a91e7b78c08c39bd1a09cd089ccd7c44410420f
Saif <146588562+SaifDwekatt@users.noreply.github.com>
2025-02-19 20:41:59 +0200

    FIRST

---
+++ takkat/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Next UI</h1>
+      <Button color="primary">Click me</Button>
+    </main>
+  )
+}
+
