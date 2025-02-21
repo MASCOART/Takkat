22c947732e8870f24c112ef83ff327d1a1f97447
marahabukh <marahabukh48@gmail.com>
2025-02-21 22:18:06 +0200

    PushFont

--- takkat/pages/_app.tsx
+++ takkat/pages/_app.tsx
@@ -1,12 +1,20 @@
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
-import { NextUIProvider } from "@nextui-org/react"
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { Noto_Kufi_Arabic } from "next/font/google"; // استيراد الخط
+
+const notoKufiArabic = Noto_Kufi_Arabic({
+  subsets: ["arabic"],
+  weight: ["100", "200", "300", "400", "500", "700", "800", "900"], // الأوزان المطلوبة
+  variable: "--font-noto-kufi",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <main className={notoKufiArabic.className}>
+        <Component {...pageProps} />
+      </main>
     </NextUIProvider>
-  )
+  );
 }
-
